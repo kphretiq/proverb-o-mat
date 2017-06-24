f1f4f3fc49a3e30de5165a11cd00c47b3908cecb
doug <doug.shawhan@gmail.com>
2017-06-24 14:07:30 -0400

    whole album

--- moody_album_name.py
+++ moody_album_name.py
@@ -3,12 +3,18 @@ import json
 import random
 
 def lambda_handler(event=None, context=None):
-    count = 30
-    proverb = []
-
     with open("moody-album-dict.json", "r") as f:
         pv = json.loads(f.read())
 
+    return {
+            "band name": artsy(pv),
+            "title": artsy(pv),
+            "tracks": [artsy(pv) for i in range(6)]
+            }
+
+def artsy(pv):
+    count = 30
+    proverb = []
     w = random.choice(list(pv.keys()))
     proverb.append(w)
     for i in range(count):
@@ -18,4 +24,4 @@ def lambda_handler(event=None, context=None):
             break
         proverb.append(w)
 
-    return {"moody album title": " ".join(proverb)}
+    return " ".join(proverb)
